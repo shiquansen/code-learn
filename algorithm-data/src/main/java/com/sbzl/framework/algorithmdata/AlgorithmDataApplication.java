fe0cd0c3982bb6cf5b344fae4f9d434d993703dc
shiquansen <k0802365@dw.gdbbk.com>
2021-01-20 11:47:31 +0800

    [整理]

---
+++ algorithm-data/src/main/java/com/sbzl/framework/algorithmdata/AlgorithmDataApplication.java
@@ -0,0 +1,13 @@
+package com.sbzl.framework.algorithmdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmDataApplication.class, args);
+    }
+
+}
---
+++ algorithm-data/src/test/java/com/sbzl/framework/algorithmdata/AlgorithmDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbzl.framework.algorithmdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
